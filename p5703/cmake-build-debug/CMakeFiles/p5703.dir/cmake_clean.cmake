efadee4acd174b54fee6f3d499f5946083005225
zzhh <691027259@qq.com>
2020-09-23 13:48:30 +0800

    p5703

---
+++ p5703/cmake-build-debug/CMakeFiles/p5703.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/p5703.dir/main.cpp.obj"
+  "libp5703.dll.a"
+  "p5703.exe"
+  "p5703.exe.manifest"
+  "p5703.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/p5703.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ p5703/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+class main{
+
+};
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
